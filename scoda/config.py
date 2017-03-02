8340991bd54a41a954656cf9a823b1684013cbd9
Matthew Adendorff <mattadendorff@gmail.com>
2017-03-02 08:22:13 +0200

    Full version

--- scoda/config.py
+++ scoda/config.py
@@ -23,13 +23,14 @@ SECURITY_PASSWORD_SALT = "sha256_crypt"
 
 # Flask-Security admin
 # SECURITY_EMAIL_SENDER = MAIL_DEFAULT_SENDER
-
 # Flask-Security URLs, overridden because they don't put a / at the end
 SECURITY_LOGIN_URL = "/login/"
+SECURITY_REGISTER_URL = "/register/"
 SECURITY_LOGOUT_URL = "/logout/"
 SECURITY_CHANGE_URL = "/change-password/"
 SECURITY_RESET_URL = "/forgot-password"
 SECURITY_POST_LOGIN_VIEW = "/dashboard"
+SECURITY_POST_REGISTER_VIEW = "/login/"
 
 # Flask-Security email subject lines
 SECURITY_EMAIL_SUBJECT_REGISTER = "Welcome to SCODA"
@@ -39,3 +40,5 @@ SECURITY_EMAIL_SENDER = "admin@opendata.durban"
 # Flask-Security features
 SECURITY_RECOVERABLE = True
 SECURITY_CHANGEABLE = True
+SECURITY_REGISTERABLE = True
+SECURITY_SEND_REGISTER_EMAIL = False
