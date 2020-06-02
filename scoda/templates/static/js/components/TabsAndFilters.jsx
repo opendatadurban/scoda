064465e44cf91015bcdd88d44dc4cbbcc8c590c5
Riaan Snyders <riaan@elevato.co.za>
2020-06-02 16:35:40 +0200

    City Dashboards
    - General Layout

--- scoda/templates/static/js/components/TabsAndFilters.jsx
+++ scoda/templates/static/js/components/TabsAndFilters.jsx
@@ -34,7 +34,7 @@ export default class TabAndFilters extends Component {
         }
     }
 
-    renderTabPane(parent) {
+    renderTabPane(parent,websiteUrl) {
 
         let tabs = [];
         let panes = [];
@@ -46,7 +46,10 @@ export default class TabAndFilters extends Component {
                         id:1,
                         value: 'People and Households',
                         mode:  'active',
-                        display:'show'
+                        display:'show',
+                        chart: 'Growth: Number of Households: 9 Major Cities',
+                        notes: 'The population of Johannesburg has doubled between 2001 and 2011. Fewer people are living in more houses which means the City has to provide infrastructure at a rate faster than the city is growing.',
+                        notesSubject: 'Johannesburg'
                     },
                     {
                         id:2,
@@ -72,7 +75,65 @@ export default class TabAndFilters extends Component {
                             <div id="title" className="col-8">{result.value}</div>
                             <div className="col-0 float-right"><i className="fas fa-download"></i></div>
                             <div className="col-2 float-right ml-1 mt-2">
-                             <div className="ds-banner-view-button">View the {parent} Website</div>
+                             <div className="ds-banner-view-button"><a href={`${websiteUrl}`} target="_new">View the {parent} Website</a></div>
+                            </div>
+                        </div>
+                        <div className="row ml-0 mt-4">
+                            <div className="col-8 ds-tabs-filter-card">
+                              <div className="row">
+                                  <div className="mt-3 ml-3 ds-tabs-filter-subheading">
+                                    {result.chart}
+                                  </div>
+                              </div>
+                              <div className="row">
+                                  <div className="mt-2 ml-3">
+                                    chart
+                                  </div>
+                              </div>
+                            </div>
+                            <div className="col">
+                                <div className="row">
+                                    <div className="col ml-3 ds-tabs-filter-card">
+                                        <div className="row">
+                                            <div className="mt-3 ml-3 ds-tabs-filter-subheading">
+                                                {result.chart}
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="mt-2 ml-3">
+                                                chart
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row mt-4">
+                                    <div className="col ml-3 ds-tabs-filter-card">
+                                        <div className="row">
+                                            <div className="mt-3 ml-3 ds-tabs-filter-subheading">
+                                                {result.chart}
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="mt-2 ml-3">
+                                                chart
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                        
+                        </div>
+                        <div className="row ml-0">
+                        <div className="col-8 ds-tabs-filter-card">
+                              <div className="row ds-filter-tabs-context-border">
+                                  <div className="mt-2 ml-3 ds-filter-tabs-context-header">
+                                    <b>Context Notes:</b> {result.notesSubject}
+                                  </div>
+                              </div>
+                              <div className="row">
+                                  <div className="ds-filter-tabs-context-content mt-3 ml-3 mb-3 mr-3">
+                                   {result.notes}
+                                  </div>
+                              </div>
                             </div>
                         </div>
                     </div>
@@ -105,7 +166,7 @@ export default class TabAndFilters extends Component {
         const tabPanes = this.props.tabs.map((result,index) =>(
             <div key={`tab-content-${result.name}`}  className={`tab-pane fade show ${result.mode}`} id={`nav-${result.name}-content`} role="tabpanel" aria-labelledby={`nav-${result.name}-tab`}>
                 <div key={`tab-container-${result.name}`} className="container-fluid">
-                    {this.renderTabPane(result.name, this.props.tabs)}
+                    {this.renderTabPane(result.name, result.url)}
                 </div>
           </div>
         ));
