abad77f9c14262bdcf250a78861004c5c4ed47cf
wasimmoosa <wcmoosa@gmail.com>
2020-06-03 11:24:55 +0200

    Added configs for production setup

--- scoda/app.py
+++ scoda/app.py
@@ -4,11 +4,16 @@ import os
 
 app = Flask(__name__, static_folder='static')
 
-env = 'production'
-app.config['ENV'] = env
-app.config.from_pyfile('config.py')
+# env = 'production'
+# app.config['ENV'] = env
+# app.config.from_pyfile('config.py')
 
 # app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = ('name', 'email')
+# setup configs
+env = os.environ.get('FLASK_ENV', 'development')
+
+app.config['ENV'] = env
+app.config.from_pyfile('config/%s.cfg' % env)
 
 # CSRF protection
 from flask_wtf.csrf import CsrfProtect
@@ -19,7 +24,7 @@ CsrfProtect(app)
 from flask_sqlalchemy import SQLAlchemy
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://scoda:scoda@localhost/scoda'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://scoda:scoda@localhost/scoda'
 db = SQLAlchemy(app)
 
 # Mail
