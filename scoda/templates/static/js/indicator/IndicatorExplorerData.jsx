ce924c862052a1c64b33ba3596438d2335898c71
wasim moosa <wcmoosa@gmail.com>
2020-06-10 11:43:07 +0200

    Added indicators api list

--- scoda/templates/static/js/indicator/IndicatorExplorerData.jsx
+++ scoda/templates/static/js/indicator/IndicatorExplorerData.jsx
@@ -4,14 +4,19 @@ import IndicatorExplorerDataCard from '../components/IndicatorExplorer.Data.Card
 
 function IndicatorData() {
   const [explorerData, setExplorerData] = useState(0);
-
+const [indicatorListData, setIndicatorListData] = useState(0);
   useEffect(() => {
     fetch('/api/explore').then(res => res.json()).then(data => {
      console.log(data);
    setExplorerData(data.cities);
     });
   }, []);
-
+  useEffect(() => {
+    fetch('/api/indicators-list').then(res => res.json()).then(data => {
+     console.log(data);
+   setIndicatorListData(data);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -21,7 +26,7 @@ function IndicatorData() {
         <p>The current Cities list is {explorerData}.</p>
 
           <p>Selecting Data:</p>
-
+    <p>The current Cities list is {indicatorListData}.</p>
       </header>
     </div>
   );
