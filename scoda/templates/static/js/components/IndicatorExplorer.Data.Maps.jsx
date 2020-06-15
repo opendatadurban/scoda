7d2cce6c17f02dcf756d1db14fe34e3e9c9cef0e
Riaan Snyders <riaan@elevato.co.za>
2020-06-15 11:39:38 +0200

    Indicator Explorer
    - Chart download functionality

--- scoda/templates/static/js/components/IndicatorExplorer.Data.Maps.jsx
+++ scoda/templates/static/js/components/IndicatorExplorer.Data.Maps.jsx
@@ -8,19 +8,12 @@ export default class IndicatorExplorerDataMap extends Component {
     }
 
     componentDidUpdate() {
-      try
-      {
-        //if(typeof this.props.geo !== undefined) {
-              this.loadGoogleVizApi(this.props.geo);
-        //}
-      }
-      catch(e) {
-        
-      }
+        if(this.props.geo.length !== 0) {
+          this.loadGoogleVizApi(this.props.geo,this.props.filterYear);
+        }
     }
   
-    loadGoogleVizApi(resultSet) {
-
+    loadGoogleVizApi(resultSet,selectedYear) {
         var options = {
             dataType: "script",
             cache: true,
@@ -32,42 +25,43 @@ export default class IndicatorExplorerDataMap extends Component {
               packages:['controls', 'bar', 'corechart', 'geochart'],
               callback: function() {
                     var dataSet = resultSet.table;
-    
-                    var selectedYear = resultSet.year;
 
                     var data = new google.visualization.DataTable();
                     
+                      data.addColumn('string',dataSet[0][0]);
                     
-                    data.addColumn('string',dataSet[0][0]);
-                
-                    for(let j=1;j<dataSet.length;j++) {
-                        let rowItem = dataSet[j];
-                        let row = [];
-                        if(rowItem[1].toString() === selectedYear) {
-                          data.addRow([rowItem[0].toString()]);
-                         }
-                    }
-                    
-                    var map = new google.visualization.ChartWrapper({
-                      'chartType': 'GeoChart',
-                      'containerId': 'map',
-                      'options': {
-                          region: 'ZA',
-                          displayMode: 'markers',
-                          resolution: 'provinces',
-                          theme: 'material',
-                          colorAxis: {colors: ['#FED976', '#FC4E2A', '#800026']},
-                          height: '100%',
-                          width:'100%',
-                          tooltip: { isHtml: true },
-                          keepAspectRatio: true
-                      }
-                  });
 
-                  map.setDataTable(data);
-          
-                  map.draw();
-                }
+                      for(let j=1;j<dataSet.length;j++) {
+
+                          let rowItem = dataSet[j];
+
+                          let row = [];
+                          if(rowItem[1].toString() === selectedYear) {
+                              data.addRow([rowItem[0].toString()]);
+                          }
+                        
+                      }
+                        
+                      var map = new google.visualization.ChartWrapper({
+                          'chartType': 'GeoChart',
+                          'containerId': 'map',
+                          'options': {
+                              region: 'ZA',
+                              displayMode: 'markers',
+                              resolution: 'provinces',
+                              theme: 'material',
+                              colorAxis: {colors: ['#FED976', '#FC4E2A', '#800026']},
+                              height: '100%',
+                              width:'100%',
+                              tooltip: { isHtml: true },
+                              keepAspectRatio: true
+                          }
+                      });
+
+                      map.setDataTable(data);
+              
+                      map.draw();
+                  }
             });
         });
     }
