069def646ba0c0768b9bddcb3ca9dbea2814b0ee
Riaan Snyders <riaan@elevato.co.za>
2020-06-18 17:29:31 +0200

    Indicator Explorer
    - Table stretch

--- scoda/templates/static/js/components/IndicatorExplorer.Data.Table.jsx
+++ scoda/templates/static/js/components/IndicatorExplorer.Data.Table.jsx
@@ -50,24 +50,6 @@ export default class IndicatorExplorerDataTable extends Component {
                         data.addRow(row);
                       }
                     }
-
-
-                    var cssClassNames = {
-                        'headerRow': 'table-header-cell',
-                        'tableRow': 'table-cell',
-                        'oddTableRow': 'table-cell',
-                        'selectedTableRow': 'table-cell',
-                        'hoverTableRow': 'table-cell',
-                        'headerCell': 'table-header-cell',
-                        'tableCell': 'table-cell',
-                        'table':'table'
-                    };
-
-                    var options = {'showRowNumber': false, 'allowHtml': true, 'cssClassNames': cssClassNames};
-                        
-                    //var table = new google.visualization.Table(document.getElementById('table'));
-
-                    //table.draw(data, options);
                 }
             });
         });
