31afd849f9ea8811f1bf0cc05a30f92df575e967
Riaan Snyders <riaan@elevato.co.za>
2020-06-29 10:20:25 +0200

    Demographics
    - start

--- scoda/templates/static/js/components/DemographicModeller.Footer.Card.jsx
+++ scoda/templates/static/js/components/DemographicModeller.Footer.Card.jsx
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 export default class DemographicModellerFooterCard extends Component {
     constructor(props) {
         super(props);
+
+        this.downloadEvent = this.downloadEvent.bind(this);
+    }
+
+    downloadEvent() {
+        this.props.downloadEvent();
     }
 
     render() {
@@ -10,17 +16,17 @@ export default class DemographicModellerFooterCard extends Component {
         return (
             <div className="mt-1 ml-5 pr-5">
                 <div className="row">
-                    <div className="col-12">
+                    <div className="col-sm-12">
                         <div className="ie-content-card">
                             <div className="col mt-1 ml-3 mr-3">
                                 <div className="row mt-3">
-                                    <div className="col-4 mt-4">
+                                    <div className="col-sm-4 mt-4">
                                         <div className="dm-footer-button-green">
                                           View Nightlights Explorer
                                         </div>
                                     </div>
                                     <div className="col mt-4 mr-4">
-                                        <div className="dm-footer-button-white float-right">
+                                        <div className="dm-footer-button-white float-right" onClick={this.downloadEvent}>
                                          Download Methodology
                                         </div>
                                     </div>
--- scoda/templates/static/js/components/DemographicModeller.jsx
+++ scoda/templates/static/js/components/DemographicModeller.jsx
@@ -6,21 +6,39 @@ import DemographicModellerFooterCard from '../components/DemographicModeller.Foo
 export default class DemographicModeller extends Component {
     constructor(props) {
         super(props);
+
+        this.downloadMethodology = this.downloadMethodology.bind(this);
+    }
+
+    downloadMethodology() {
+        let fileName = 'report.pdf';
+        let encodedUri = '/static/dist/data/report.pdf';
+        this.downloadData(encodedUri,fileName);
+    }
+
+    downloadData(uri,filename) {
+        let link = document.createElement("a");
+        link.download = filename;
+        link.href = uri;
+        link.target = '_blank';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     }
 
     render() {
-
         return (
-            <div className="container px-5">
+            <div className="container">
                <div className="row">
-                   <div className="col-12">
+                   <div className="col-sm-12">
                         <DemographicModellerDataCard
                         />
                    </div>
                </div>
                <div className="row">
-                   <div className="col-12">
-                       <DemographicModellerFooterCard />
+                   <div className="col-sm-12">
+                       <DemographicModellerFooterCard 
+                       downloadEvent={this.downloadMethodology}/>
                    </div>
                </div>
             </div>
