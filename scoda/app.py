8373c2d0afcea2b04cfde2cceecf2d98e7ea5a00
wasim moosa <wcmoosa@gmail.com>
2020-07-07 14:57:22 +0200

    Update app.py

--- scoda/app.py
+++ scoda/app.py
@@ -6,11 +6,10 @@ import os
 app = Flask(__name__, static_folder='static')
 
 # setup configs
-env = 'production'
-app.config['ENV'] = env
-app.config.from_pyfile('config.py')
+env = os.environ.get('FLASK_ENV', 'development')
 
-#app.config.from_pyfile('config/%s.cfg' % env)
+app.config['ENV'] = env
+app.config.from_pyfile('config/%s.cfg' % env)
 # app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = ('name', 'email')
 
 # CSRF protection
@@ -22,7 +21,7 @@ csrf = CsrfProtect(app)
 from flask_sqlalchemy import SQLAlchemy
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://scoda:scoda@localhost/scoda'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://scoda:scoda@localhost/scoda'
 db = SQLAlchemy(app)
 
 # Mail
@@ -33,3 +32,4 @@ mail = Mail(app)
 from flask_mobility import Mobility
 
 mobility = Mobility(app)
+
