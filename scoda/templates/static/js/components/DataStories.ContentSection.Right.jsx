3c21ed728dde61f0ac8d684abbd11304ec51c0d2
xulucreatives <xulusah1998@gmail.com>
2020-09-30 10:21:40 +0200

    scoda QA R3

--- scoda/templates/static/js/components/DataStories.ContentSection.Right.jsx
+++ scoda/templates/static/js/components/DataStories.ContentSection.Right.jsx
@@ -29,28 +29,29 @@ export default class ContentSectionRight extends Component {
             position:'relative',
             top:'42px',
             left:'8px',
-            paddingTop:'1px',
+            paddingTop:'0.2px',
+            paddingRight:'1.3px'
         };
 
         return (
             <div className={main_class}>
                 <a href={this.props.href_} className="card-trigger"></a>
                <div className="container row m-0">
-                   <div className="col-md-8 p-0 ds-content-image">
+                   <div className="col-md-6 col-lg-8 col-xl-8 p-0 ds-content-image">
                        <div style={filterTag}>{this.props.imageTag}</div>
                        <Image imgSrc={sectionImage} className="ds-content-image-big" />
                    </div>
                    <div className="col-md-4 ds-right-content-width m-0">
                        <div className="row m-0">
-                           <div className="col-0 mt-2 ds-content-title"><br/>{this.props.title}</div>
+                           <div className="col-0 mt-2 pl-3 ds-content-title"><br/>{this.props.title}</div>
                        </div>
                        <div className="ds-right-content-hspacer"></div>
                        <div className="row m-0">
-                           <div className="col-0 ds-content-subtitle">{this.props.subtitle}</div>
+                           <div className="col-0 pl-3 ds-content-subtitle">{this.props.subtitle}</div>
                        </div>
                        <div className="ds-right-content-hspacer"></div>
                        <div className="row m-0">
-                            <div className="col-0 ds-content">{this.props.content}</div>
+                            <div className="col-0 p-3 ds-content">{this.props.content}</div>
                        </div>
                        <div className="ds-right-content-hspacer"></div>
                        <div className="row m-0">
