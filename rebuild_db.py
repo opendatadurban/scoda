525588554683b64010126394921ba429b7f38b68
wasim moosa <wcmoosa@gmail.com>
2020-10-14 20:50:35 +0200

    Added codebook metadata

--- rebuild_db.py
+++ rebuild_db.py
@@ -4,13 +4,14 @@ from scoda.models.seeds_JHBGrid import seed_db as seed_jhb_grid
 from scoda.models.seeds_ETH import seed_db as seed_dbn
 from scoda.models.seeds import seed_db as seed_jhb
 
-db.drop_all()
-db.create_all()
-seed_jhb(db)
-seed_dbn(db)
-seed_jhb_grid(db)
-seed_eth_grid(db)
+# db.drop_all()
+# db.create_all()
+# seed_jhb(db)
+# seed_dbn(db)
+# seed_jhb_grid(db)
+# seed_eth_grid(db)
 
-from scoda.models.seed_codebook import seed_codebook_data
+from scoda.models.seed_codebook import seed_codebook_data,seed_indicator_data
 
-seed_codebook_data(db)
+# seed_codebook_data(db)
+seed_indicator_data(db)
