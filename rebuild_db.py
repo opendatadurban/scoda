ce9b7a194eb52b529533a0067994b10d5eddf3b7
wasim moosa <wcmoosa@gmail.com>
2020-10-30 11:41:09 +0200

    Updated requirements.txt and added search vector
    Added new sdg & c88 tables WIP
    Added new Searchable requirements and updated sqlalchemy
    Tweaked the rebuild for staging

--- rebuild_db.py
+++ rebuild_db.py
@@ -5,13 +5,15 @@ from scoda.models.seeds_ETH import seed_db as seed_dbn
 from scoda.models.seeds import seed_db as seed_jhb
 
 db.drop_all()
+db.configure_mappers()
 db.create_all()
-seed_jhb(db)
-seed_dbn(db)
-seed_jhb_grid(db)
-seed_eth_grid(db)
+# seed_jhb(db)
+# seed_dbn(db)
+# seed_jhb_grid(db)
+# seed_eth_grid(db)
 
-from scoda.models.seed_codebook import seed_codebook_data,seed_indicator_data
+from scoda.models.seed_codebook import seed_codebook_data,seed_indicator_data,seed_indicator_json
 
 seed_codebook_data(db)
 seed_indicator_data(db)
+# seed_indicator_json(db)
