c3df0a3534a0fd792434a6cbc31fd38a7e70f1ca
John Johnson <jpj9307@gmail.com>
2020-11-05 12:00:04 +0200

    changed filter label names

--- scoda/templates/static/js/data/filterData.js
+++ scoda/templates/static/js/data/filterData.js
@@ -92,67 +92,67 @@ const filterData = [
       "value": "All",
       "children": [
         {
-          "label": "Goal 1. End poverty in all its forms everywhere",
+          "label": "Goal 1.",
           "value": "Goal 1. End poverty in all its forms everywhere",
           "selected": true
         },
         {
-          "label": "Goal 2. End hunger, achieve food security and improved nutrition and promote sustainable agriculture",
+          "label": "Goal 2.",
           "value": "Goal 2. End hunger, achieve food security and improved nutrition and promote sustainable agriculture",
           "selected": true
         },
         {
-          "label": "Goal 3. Ensure healthy lives and promote well-being for all at all ages",
+          "label": "Goal 3.",
           "value": "Goal 3. Ensure healthy lives and promote well-being for all at all ages",
           "selected": true
         },
         {
-          "label": "Goal 4. Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all",
+          "label": "Goal 4.",
           "value": "Goal 4. Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all",
           "selected": true
         },
         {
-          "label": "Goal 5",
-          "value": "Goal 5",
+          "label": "Goal 5.",
+          "value": "Goal 5.",
           "selected": true
         },
         {
-          "label": "Goal 6. Ensure availability and sustainable management of water and sanitation for all",
+          "label": "Goal 6.",
           "value": "Goal 6. Ensure availability and sustainable management of water and sanitation for all",
           "selected": true
         },
         {
-          "label": "Goal 7. Ensure access to affordable, reliable, sustainable and modern energy for all",
+          "label": "Goal 7.",
           "value": "Goal 7. Ensure access to affordable, reliable, sustainable and modern energy for all",
           "selected": true
         },
         {
-          "label": "Goal 8. Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all",
+          "label": "Goal 8.",
           "value": "Goal 8. Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all",
           "selected": true
         },
         {
-          "label": "Goal 9. Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation",
+          "label": "Goal 9.",
           "value": "Goal 9. Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation",
           "selected": true
         },
         {
-          "label": "Goal 10. Reduce inequality within and among countries",
+          "label": "Goal 10.",
           "value": "Goal 10. Reduce inequality within and among countries",
           "selected": true
         },
         {
-          "label": "Goal 11. Make cities and human settlements inclusive, safe, resilient and sustainable",
+          "label": "Goal 11.",
           "value": "Goal 11. Make cities and human settlements inclusive, safe, resilient and sustainable",
           "selected": true
         },
         {
-          "label": "Goal 12. Ensure sustainable consumption and production patterns",
+          "label": "Goal 12.",
           "value": "Goal 12. Ensure sustainable consumption and production patterns",
           "selected": true
         },
         {
-          "label": "Goal 13. Take urgent action to combat climate change and its impacts",
+          "label": "Goal 13.",
           "value": "Goal 13. Take urgent action to combat climate change and its impacts",
           "selected": true
         },
@@ -162,12 +162,12 @@ const filterData = [
           "selected": true
         },
         {
-          "label": "Goal 15. Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss",
+          "label": "Goal 15.",
           "value": "Goal 15. Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss",
           "selected": true
         },
         {
-          "label": "Goal 16. Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels",
+          "label": "Goal 16.",
           "value": "Goal 16. Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels",
           "selected": true
         }
