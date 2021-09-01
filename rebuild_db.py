ae6a0821f2e17c422dfc3056a45178e9ce70962a
wasim moosa <wcmoosa@gmail.com>
2021-09-01 22:18:53 +0200

    Updated codebook seed to include total number of households in the municipality data

--- rebuild_db.py
+++ rebuild_db.py
@@ -1,19 +1,19 @@
 from scoda.models import db
-from scoda.models.seeds_ETHGrid import seed_db as seed_eth_grid
-from scoda.models.seeds_JHBGrid import seed_db as seed_jhb_grid
-from scoda.models.seeds_ETH import seed_db as seed_dbn
-from scoda.models.seeds import seed_db as seed_jhb
+# from scoda.models.seeds_ETHGrid import seed_db as seed_eth_grid
+# from scoda.models.seeds_JHBGrid import seed_db as seed_jhb_grid
+# from scoda.models.seeds_ETH import seed_db as seed_dbn
+# from scoda.models.seeds import seed_db as seed_jhb
 
 db.drop_all()
 db.configure_mappers()
 db.create_all()
-seed_jhb(db)
-seed_dbn(db)
-seed_jhb_grid(db)
-seed_eth_grid(db)
+# seed_jhb(db)
+# seed_dbn(db)
+# seed_jhb_grid(db)
+# seed_eth_grid(db)
 
-from scoda.models.seed_codebook import seed_codebook_data,seed_indicator_data
+from scoda.models.seed_codebook import seed_codebook_data,seed_indicator_data,seed_total_households_municipality
 
 seed_indicator_data(db)
 seed_codebook_data(db)
-
+seed_total_households_municipality(db)
