781ab52a8b3dcb6e9f607d2fb575e2fa9b470134
Ayanda Nene <ayanda@black-box.io>
2021-09-23 15:19:33 +0200

    scroll from the top

--- scoda/templates/static/js/components/DemographicModeller.jsx
+++ scoda/templates/static/js/components/DemographicModeller.jsx
@@ -41,6 +41,13 @@ export default class DemographicModeller extends Component {
         document.body.removeChild(link);
     }
 
+    componentDidMount(){
+        if ('scrollRestoration' in history) {
+            history.scrollRestoration = 'manual';
+          }
+          // This is needed if the user scrolls down during page load and you want to make sure the page is scrolled to the top once it's fully loaded. This has Cross-browser support.
+          window.scrollTo(0,0);
+    }
     render() {
         return (
             <div className="container pb-5">
