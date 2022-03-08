9bd95bc39a14f83505014abdd494f76d5d9e9a4a
PhakamaniXulu <66253043+PhakamaniXulu@users.noreply.github.com>
2022-03-08 14:06:33 +0200

    dropdown item as props

--- scoda/templates/static/js/components/Socr.Dashboards.jsx
+++ scoda/templates/static/js/components/Socr.Dashboards.jsx
@@ -4,13 +4,12 @@ import Subnav from './Subnav'
 import Stat_Panel from './Statistics.Display.Panel'
 import Charts_dashboards from './Charts.Dashboards'
 
-
 export default class Socr_dashboards extends Component {
     render(){
         return(
         <div className='sorc_dashboards'>
             <div className='spacer--top'></div>
-              <Subnav name='State of Cities Reports' dropdownName='Service Delivery' buttonText="Download as PNG"/>
+              <Subnav name='State of Cities Reports' dropdownName='Service Delivery' dropDownItem='Service Delivery' buttonText="Download as PNG"/>
               <Sidebar_left/>
               <div id='content'>
                <Stat_Panel/>
