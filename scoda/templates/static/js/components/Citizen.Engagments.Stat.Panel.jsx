44daa32ed6a5b0192165d89fe8e78c6e0d539c1a
PhakamaniXulu <66253043+PhakamaniXulu@users.noreply.github.com>
2022-03-08 14:06:03 +0200

    citizen engagement charts

---
+++ scoda/templates/static/js/components/Citizen.Engagments.Stat.Panel.jsx
@@ -0,0 +1,157 @@
+import React, { Component,useEffect, useState } from 'react'
+import Select from './Select'
+import axios from 'axios';
+
+const CitizenEngagmentStatPanel = () => {
+
+    const [place,setPlace] = useState('Buffalo City')
+    const [voter_turnout_national, setVoter_turnout_national] = useState(40)
+    const [registered_national, setRegistered_national] = useState(64)
+    const [voter_turnout_local, setVoter_turnout_local] = useState(51)
+    const [registered_local, setRegistered_local] = useState(62)
+
+    const mydata = {
+        pl: {
+            id: "my-select",
+            multiple: false,
+            data: ['Buffalo City', 'City of Cape Town', 'City of Joburg', 'Ekurhuleni', 'eThekwini', 'Mangaung', 'Nelson Mandela Bay', 'Tshwane']
+        }
+    }
+    const average = [
+        {
+            percentage: 42,
+            type: `VOTER TURNOUT<br/>NATIONAL`
+        },
+        {
+            percentage: 62,
+            type: `REGISTERED<br/>NATIONAL`
+        },
+        {
+            percentage: 57,
+            type: `VOTER TURNOUT<br/>LOCAL`
+        },
+        {
+            percentage: 68,
+            type: `REGISTERED<br/>LOCAL`
+        }
+    ]
+    let focus = [
+        {
+            percentage: voter_turnout_national,
+            type: `VOTER TURNOUT<br/>NATIONAL`
+        },
+        {
+            percentage: registered_national,
+            type: `REGISTERED<br/>NATIONAL`
+        },
+        {
+            percentage: voter_turnout_local,
+            type: `VOTER TURNOUT<br/>LOCAL`
+        },
+        {
+            percentage: registered_local,
+            type: `REGISTERED<br/>LOCAL`
+        }
+    ]
+    useEffect(() => {
+         console.log(average[0].percentage)
+      });
+    
+    const places = (e) => {
+        setPlace(e.target.value);
+        console.log('selected city :',e.target.value)
+        if(e.target.value ==='Buffalo City'){
+            setVoter_turnout_national(40)
+            setRegistered_national(64)
+            setVoter_turnout_local(51) 
+            setRegistered_local(62)
+        }
+        if(e.target.value ==='City of Cape Town'){
+            setVoter_turnout_national(12)
+            setRegistered_national(32)
+            setVoter_turnout_local(56) 
+            setRegistered_local(43)
+        }
+        if(e.target.value ==='City of Joburg'){
+            setVoter_turnout_national(34)
+            setRegistered_national(89)
+            setVoter_turnout_local(56) 
+            setRegistered_local(43)
+        }
+        if(e.target.value ==='Ekurhuleni'){
+            setVoter_turnout_national(45)
+            setRegistered_national(34)
+            setVoter_turnout_local(75) 
+            setRegistered_local(56)
+        }
+        if(e.target.value ==='eThekwini'){
+            setVoter_turnout_national(12)
+            setRegistered_national(34)
+            setVoter_turnout_local(34) 
+            setRegistered_local(56)
+        }
+        if(e.target.value ==='Mangaung'){
+            setVoter_turnout_national(90)
+            setRegistered_national(54)
+            setVoter_turnout_local(34) 
+            setRegistered_local(67)
+        }
+        if(e.target.value ==='Nelson Mandela Bay'){
+            setVoter_turnout_national(34)
+            setRegistered_national(45)
+            setVoter_turnout_local(56) 
+            setRegistered_local(64)
+        }
+        if(e.target.value ==='Tshwane'){
+            setVoter_turnout_national(23)
+            setRegistered_national(42)
+            setVoter_turnout_local(12) 
+            setRegistered_local(76)
+        }
+    }
+
+    return (
+        <div className='stat_display_panel'>
+            <div className='row stat_display_panel--numbers w-100'>
+                <div className='col-md-6 first_panel'>
+                    <p className='catagory-name '>Averages 2019 <span>(Voting and Registration)</span></p>
+                    <div className='row'>
+                        {average.map((item, i) => (
+                            <div className='col p-0'>
+                                <h1>{item.percentage}%</h1>
+                                <p dangerouslySetInnerHTML={{__html:item.type}}></p>
+                            </div>
+                        )
+                        )}
+                    </div>
+                </div>
+                <div className='col-md-6'>
+                    <div className='row'>
+                        <div className='col-md-8'> <p className='catagory-name '>Municipality Focus 2018: {place}</p></div>
+                        <div className='col-md-3'>
+                            <Select
+                                value={place}
+                                placeholder='Select a City'
+                                id={mydata.pl.id}
+                                multiple={mydata.pl.multiple}
+                                data={mydata.pl.data}
+                                name='gender'
+                                onChange={places}
+                            /></div>
+                    </div>
+                    <div className='row'>
+                        {focus.map((item, i) => (
+                            <div className='col'>
+                                <h1 className={item.percentage > average[i].percentage ? 'green' : item.percentage < average[i].percentage ? 'red' : 'none'}>{item.percentage}%</h1>
+                                <p dangerouslySetInnerHTML={{__html:item.type}}></p>
+                            </div>
+                        )
+                        )}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default CitizenEngagmentStatPanel;
---
+++ scoda/templates/static/js/components/Socr.Citizens.Engagement.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Sidebar_left from './Sidebar_left'
+import Subnav from './Subnav'
+import Stat_Panel from './Citizen.Engagments.Stat.Panel'
+import Charts_dashboards from './Charts.Citizen.Engagements'
+
+export default class Socr_Citizens_Engagement extends Component {
+    render() {
+        return (
+            <div className='sorc_dashboards'>
+                <div className='spacer--top'></div>
+                <Subnav name='State of Cities Reports' dropdownName='Citizen Engagement' dropDownItem='Citizen Engagement' buttonText="Download as PNG" />
+                <Sidebar_left />
+                <div id='content'>
+                <Stat_Panel/>
+                <Charts_dashboards/>
+                </div>
+            </div>
+        )
+    }
+}
