586360b57638a862fb219cef58ad99d778d5665f
PhakamaniXulu <66253043+PhakamaniXulu@users.noreply.github.com>
2022-03-08 14:06:48 +0200

    download loader

--- scoda/templates/static/js/components/DownloadScreenCapture.jsx
+++ scoda/templates/static/js/components/DownloadScreenCapture.jsx
@@ -1,31 +1,48 @@
-import React, { Component } from 'react';
-import Button  from './Button';
+import React, { useState } from 'react';
+import Button from './Button';
+import { Modal, ModalBody, Spinner } from 'reactstrap';
 
+const DownloadScreenCapture = (props) => {
 
-export default class DownloadScreenCapture extends Component {
-  constructor(props){
-    super(props);
-    this.download = this.download.bind(this);
-  }
+  const [loader, setLoader] = useState(false)
+  const download = (e) => {
 
-
-  download(e){
+    setLoader(true)
     var htmlToImage = require('html-to-image');
     e.preventDefault()
     htmlToImage.toJpeg(document.getElementById('content-scoda'), { quality: 0.95 })
-  .then(function (dataUrl) {
-    var link = document.createElement('a');
-    link.download = 'data-snapshot.jpeg';
-    link.href = dataUrl;
-    link.click();
-  });
+      .then(function (dataUrl) {
+        setLoader(false)
+        var link = document.createElement('a');
+        link.download = 'data-snapshot.jpeg';
+        link.href = dataUrl;
+        link.click();
+      });
 
   }
+  return (
+    <>
+      <Button className={["download-screen-capture "] + props.className} onClick={download} text={props.buttonText} id="download" />
+      {loader ?
+        <Modal id="loader" isOpen={loader} className="modal-dialog-centered loader">
+          <ModalBody>
+            <div className="row">
+              <div className="col-2"></div>
+              <div className="col-0 ml-3 pt-4">
+                <Spinner type="grow" color="secondary" size="sm" />
+                <Spinner type="grow" color="success" size="sm" />
+                <Spinner type="grow" color="danger" size="sm" />
+                <Spinner type="grow" color="warning" size="sm" />
+              </div>
+              <div className="col-0 pt-4 pl-4 float-left">Downloading Image...</div>
+            </div>
+            <br />
+          </ModalBody>
+        </Modal>
+        : ''
+      }
+    </>
 
-  render(){
-    return (
-      <Button className={["download-screen-capture "] + this.props.className} onClick={this.download} text={this.props.buttonText} id="download"/>
-    );
-  }
-
-};
+  );
+}
+export default DownloadScreenCapture;
