7e277f239aa8dff2e73c4c320e4febd138c9a8f5
PhakamaniXulu <66253043+PhakamaniXulu@users.noreply.github.com>
2022-03-11 08:48:44 +0200

    routes and stylings

--- scoda/templates/static/js/socr.js
+++ scoda/templates/static/js/socr.js
@@ -7,6 +7,7 @@ import loader from "./gif/Spinner.gif"
 const SoCR =  lazy(() => import('./templates/SoCR'));
 const CitizenEngagements =  lazy(() => import('./templates/CitizenEngagements'));
 const Socr_Dashboards =  lazy(() => import('./templates/SocrDashboard'));
+const HumanResources =  lazy(() => import('./templates/HumanResources'));
 
 const style = {
       position: 'absolute', left: '50%', top: '50%',
@@ -27,6 +28,7 @@ export default (
       <Route exact path='/' component={SoCR} />
       <Route exact path='/citizen-engagement' component={CitizenEngagements} />
       <Route exact path='/service-delivery' component={Socr_Dashboards} />
+      <Route exact path='/human-resources' component={HumanResources} />
 
       </Suspense>
     </HashRouter>
