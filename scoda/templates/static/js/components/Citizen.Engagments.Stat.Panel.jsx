869a9102f6b13b7befc589b68d1098995687eaea
PhakamaniXulu <66253043+PhakamaniXulu@users.noreply.github.com>
2022-03-11 08:48:27 +0200

    updated data and variable names

--- scoda/templates/static/js/components/Citizen.Engagments.Stat.Panel.jsx
+++ scoda/templates/static/js/components/Citizen.Engagments.Stat.Panel.jsx
@@ -7,9 +7,9 @@ import panelData from '../data/panelData.js';
 const CitizenEngagmentStatPanel = () => {
 
     const [place,setPlace] = useState('Buffalo City')
-    const [voter_turnout_national, setVoter_turnout_national] = useState(40)
+    const [total_number_of_municipal_posts_national, settotal_number_of_municipal_posts_national] = useState(40)
     const [registered_national, setRegistered_national] = useState(64)
-    const [voter_turnout_local, setVoter_turnout_local] = useState(51)
+    const [municipal_management_vacancies, setmunicipal_management_vacancies] = useState(51)
     const [registered_local, setRegistered_local] = useState(62)
     //set data
     const [data,setData] = useState(panelData[0])
@@ -18,7 +18,7 @@ const CitizenEngagmentStatPanel = () => {
     
     let municipality_focus = [
         {
-            percentage: voter_turnout_national,
+            percentage: total_number_of_municipal_posts_national,
             type: `VOTER TURNOUT<br/>NATIONAL`
         },
         {
@@ -26,7 +26,7 @@ const CitizenEngagmentStatPanel = () => {
             type: `REGISTERED<br/>NATIONAL`
         },
         {
-            percentage: voter_turnout_local,
+            percentage: municipal_management_vacancies,
             type: `VOTER TURNOUT<br/>LOCAL`
         },
         {
@@ -39,51 +39,51 @@ const CitizenEngagmentStatPanel = () => {
         setPlace(e.target.value);
         console.log('selected city :',e.target.value)
         if(e.target.value ==='Buffalo City'){
-            setVoter_turnout_national(40)
+            settotal_number_of_municipal_posts_national(40)
             setRegistered_national(64)
-            setVoter_turnout_local(51) 
+            setmunicipal_management_vacancies(51) 
             setRegistered_local(62)
         }
         if(e.target.value ==='City of Cape Town'){
-            setVoter_turnout_national(12)
+            settotal_number_of_municipal_posts_national(12)
             setRegistered_national(32)
-            setVoter_turnout_local(56) 
+            setmunicipal_management_vacancies(56) 
             setRegistered_local(43)
         }
         if(e.target.value ==='City of Joburg'){
-            setVoter_turnout_national(34)
+            settotal_number_of_municipal_posts_national(34)
             setRegistered_national(89)
-            setVoter_turnout_local(56) 
+            setmunicipal_management_vacancies(56) 
             setRegistered_local(43)
         }
         if(e.target.value ==='Ekurhuleni'){
-            setVoter_turnout_national(45)
+            settotal_number_of_municipal_posts_national(45)
             setRegistered_national(34)
-            setVoter_turnout_local(75) 
+            setmunicipal_management_vacancies(75) 
             setRegistered_local(56)
         }
         if(e.target.value ==='eThekwini'){
-            setVoter_turnout_national(12)
+            settotal_number_of_municipal_posts_national(12)
             setRegistered_national(34)
-            setVoter_turnout_local(34) 
+            setmunicipal_management_vacancies(34) 
             setRegistered_local(56)
         }
         if(e.target.value ==='Mangaung'){
-            setVoter_turnout_national(90)
+            settotal_number_of_municipal_posts_national(90)
             setRegistered_national(54)
-            setVoter_turnout_local(34) 
+            setmunicipal_management_vacancies(34) 
             setRegistered_local(67)
         }
         if(e.target.value ==='Nelson Mandela Bay'){
-            setVoter_turnout_national(34)
+            settotal_number_of_municipal_posts_national(34)
             setRegistered_national(45)
-            setVoter_turnout_local(56) 
+            setmunicipal_management_vacancies(56) 
             setRegistered_local(64)
         }
         if(e.target.value ==='Tshwane'){
-            setVoter_turnout_national(23)
+            settotal_number_of_municipal_posts_national(23)
             setRegistered_national(42)
-            setVoter_turnout_local(12) 
+            setmunicipal_management_vacancies(12) 
             setRegistered_local(76)
         }
     }
