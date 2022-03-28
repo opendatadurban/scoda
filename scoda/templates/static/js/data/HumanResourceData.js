58830311770221dbdecbdf152099f02a69f77ca1
PhakamaniXulu <66253043+PhakamaniXulu@users.noreply.github.com>
2022-03-28 09:18:13 +0200

    codebook links

--- scoda/templates/static/js/data/HumanResourceData.js
+++ scoda/templates/static/js/data/HumanResourceData.js
@@ -2,62 +2,62 @@ const postBreakdown = [
     {
         "department" : "Community & Social Services",
         "codebook_id" : "GG.SOCR.28.1",
-        "link":""
+        "link":"toolkit#/codebook-explorer/971"
     },
     {
         "department" : "Financial & Administration",
         "codebook_id" : "GG.SOCR.28.2",
-        "link":""
+        "link":"toolkit#/codebook-explorer/972"
     },
     {
         "department" : "Electricity",
         "codebook_id" : "GG.SOCR.28.3",
-        "link":""
+        "link":"toolkit#/codebook-explorer/973"
     },
     {
         "department" : "Environmental Protection",
         "codebook_id" : "GG.SOCR.28.4",
-        "link":""
+        "link":"toolkit#/codebook-explorer/974"
     },
     {
         "department" : "Health",
         "codebook_id" : "GG.SOCR.28.5",
-        "link":""
+        "link":"toolkit#/codebook-explorer/975"
     },
     {
         "department" : "Public Safety",
         "codebook_id" : "GG.SOCR.28.6",
-        "link":""
+        "link":"toolkit#/codebook-explorer/976"
     },
     {
         "department" : "Road Transport",
         "codebook_id" : "GG.SOCR.28.7",
-        "link":""
+        "link":"toolkit#/codebook-explorer/977"
     },
     {
         "department" : "Sport & Recreation",
         "codebook_id" : "GG.SOCR.28.8",
-        "link":""
+        "link":"toolkit#/codebook-explorer/978"
     },
     {
         "department" : "Waste Management",
         "codebook_id" : "GG.SOCR.28.9",
-        "link":""
+        "link":"toolkit#/codebook-explorer/979"
     },
     {
         "department" : "Waste Water Management",
         "codebook_id" : "GG.SOCR.28.10",
-        "link":""
+        "link":"toolkit#/codebook-explorer/980"
     },
     {
         "department" : "Water",
         "codebook_id" : "GG.SOCR.28.11",
-        "link":""
+        "link":"toolkit#/codebook-explorer/981"
     },
     {
         "department" : "Other",
         "codebook_id" : "GG.SOCR.28.12",
-        "link":""
+        "link":"toolkit#/codebook-explorer/982"
     }
   ];
   
