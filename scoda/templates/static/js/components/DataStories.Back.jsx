89f1bca47b413f34538c6d1ebeb55617a61fe896
PhakamaniXulu <66253043+PhakamaniXulu@users.noreply.github.com>
2022-05-30 15:43:40 +0200

    back button component

---
+++ scoda/templates/static/js/components/DataStories.Back.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import DataStoriesSVGIcon from '../components/DataStories.Icon.Svg';
+
+export default class DataStoriesNavigation extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        let homeUrl = '/scoda/socr#/';
+
+        return (
+            <div className="container-fluid mt-3">
+               <div className="row">
+                    <div className="col-0 ml-5 ds-main-navigation-button-back2 ml-0" onClick={()=>window.location = `${homeUrl}`}>
+                        <div className="row">
+                            <div className="col-2"></div>
+                            <div className="col-0 ds-main-navigation-button-back-icon"><DataStoriesSVGIcon/></div>
+                            <div className="col-0 ds-main-navigation-button-back-text">Back to Home</div>
+                       </div>
+                    </div>
+                    <div className="col"></div>
+                </div>
+               <div className="row">
+                   <div className="h-25 d-inline-block"></div>
+               </div>
+            </div>
+        )
+    }
+}
