3f4642b5a37e5a7c021c3746b93b5d5ad1a187ca
Nzuzo_Malinga <nzuzomal@gmail.com>
2022-07-20 14:41:25 +0200

    legend padding

--- scoda/templates/static/js/components/GenericDashboardSetup/Chart.jsx
+++ scoda/templates/static/js/components/GenericDashboardSetup/Chart.jsx
@@ -1,8 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 
 import { BarChartGeneric } from '../BarChartGeneric';
 
-export const Chart = ({ graphData, labels,title }) => {
+export const Chart = ({ graphData, labels, title }) => {
+
+    const [windowSize, setWindowSize] = useState(getWindowSize());
+
+    useEffect(() => {
+        function handleWindowResize() {
+            setWindowSize(getWindowSize());
+        }
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
 
 
     const options = {
@@ -11,7 +25,11 @@ export const Chart = ({ graphData, labels,title }) => {
         legend: {
             display: true,
             labels: {
-                fontSize: 8
+                fontSize: 8,
+                boxWidth: 25,
+                fontColor: 'rgba(74, 74, 74, 1)',
+                fontFamily: 'Montserrat',
+                padding: (40/1140) * windowSize.innerWidth
             },
             onHover: function (e) {
                 e.target.style.cursor = 'pointer';
@@ -19,11 +37,11 @@ export const Chart = ({ graphData, labels,title }) => {
         },
         scales: {
             xAxes: [{
-               
+
                 gridLines: {
                     display: false
                 },
-                ticks: { fontFamily: "Montserrat", fontSize: 8, fontStyle:'600' },
+                ticks: { fontFamily: "Montserrat", fontSize: 8, fontStyle: '600' },
                 gridLines: {
                     display: false,
                 }
@@ -32,10 +50,10 @@ export const Chart = ({ graphData, labels,title }) => {
                 scaleLabel: {
                     display: true,
                     labelString: title,
-                    fontFamily: "Montserrat", 
-                    fontSize: 8, 
-                    fontStyle:'600' 
-                  },
+                    fontFamily: "Montserrat",
+                    fontSize: 8,
+                    fontStyle: '600'
+                },
                 gridLines: {
                     display: false
                 },
@@ -45,29 +63,34 @@ export const Chart = ({ graphData, labels,title }) => {
     };
 
     const data = {
-        labels:  graphData[0].chartData ? labels[0].labels :labels,
-        datasets: graphData[0].chartData?
+        labels: graphData[0].chartData ? labels[0].labels : labels,
+        datasets: graphData[0].chartData ?
 
-        graphData.map( (item, index)=> {
-            return {
-                label: item.chartData.label,
-                data: item.chartData.values,
-                backgroundColor: item.chartData.color
-            }
-        })
-        
-        :graphData.map( (item, index)=> {
-            return {
-                label: item.year,
-                data: item.values,
-                backgroundColor: item.color
-            }
-        })
+            graphData.map((item, index) => {
+                return {
+                    label: item.chartData.label,
+                    data: item.chartData.values,
+                    backgroundColor: item.chartData.color
+                }
+            })
+
+            : graphData.map((item, index) => {
+                return {
+                    label: item.year,
+                    data: item.values,
+                    backgroundColor: item.color
+                }
+            })
     }
-    console.log(data,"generic chart data")
+    console.log(data, "generic chart data")
     return (
         <>
             <BarChartGeneric options={options} data={data} />
         </>
     )
 }
+
+function getWindowSize() {
+    const {innerWidth, innerHeight} = window;
+    return {innerWidth, innerHeight};
+  }
