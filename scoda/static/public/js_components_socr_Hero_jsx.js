2f31bac31ecfde007fe6f4b61d451ffbdbeeeefa
Nzuzo_Malinga <nzuzomal@gmail.com>
2023-01-24 17:12:09 +0200

    build files and coonfig for dashboard rebuild

---
+++ scoda/static/public/js_components_socr_Hero_jsx.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkocl_bb_library"] = self["webpackChunkocl_bb_library"] || []).push([["js_components_socr_Hero_jsx"],{
+
+/***/ "./js/components/socr/Hero.jsx":
+/*!*************************************!*\
+  !*** ./js/components/socr/Hero.jsx ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nvar Hero = function Hero(_ref) {\n  var setIsOpen = _ref.setIsOpen;\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", {\n    className: \"socr--hero\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", {\n    className: \"container\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", {\n    className: \"row\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", {\n    className: \"col-md-5 hero--image\"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"img\", {\n    src: \"/static/dist/img/image_hero-banner.webp\",\n    alt: \"banner\"\n  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", {\n    className: \"col-md-7 hero--content \"\n  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"h5\", {\n    className: \"hero--title\"\n  }, \"State of Cities Report\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"p\", {\n    className: \"hero--p\"\n  }, \"State of Cities Reports produce solid and up-to-date information that helps inform and support the work of urban actors across different sectors of society, as well as research into how to make South African cities functional.\"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", {\n    className: \"hero--btn\",\n    onClick: function onClick(e) {\n      setIsOpen(true);\n    }\n  }, \"View Reports\")))));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Hero);\n\n//# sourceURL=webpack://ocl_bb_library/./js/components/socr/Hero.jsx?");
+
+/***/ })
+
+}]);
---
+++ scoda/static/public/js_templates_datastories_DatastoryHouseholdBills_jsx.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkocl_bb_library"] = self["webpackChunkocl_bb_library"] || []).push([["js_templates_datastories_DatastoryHouseholdBills_jsx"],{
+
+/***/ "./js/templates/datastories/DatastoryHouseholdBills.jsx":
+/*!**************************************************************!*\
+  !*** ./js/templates/datastories/DatastoryHouseholdBills.jsx ***!
+  \**************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nvar Navigation_scoda = /*#__PURE__*/(0,react__WEBPACK_IMPORTED_MODULE_0__.lazy)(function () {\n  return Promise.all(/*! import() */[__webpack_require__.e(\"vendors-node_modules_jquery_dist_jquery_js\"), __webpack_require__.e(\"js_components_Navigation_scoda_jsx\")]).then(__webpack_require__.bind(__webpack_require__, /*! ../../components/Navigation_scoda */ \"./js/components/Navigation_scoda.jsx\"));\n});\nvar DataStory = /*#__PURE__*/(0,react__WEBPACK_IMPORTED_MODULE_0__.lazy)(function () {\n  return Promise.all(/*! import() */[__webpack_require__.e(\"js_components_DataStories_Back_jsx-js_components_Iframe_jsx\"), __webpack_require__.e(\"js_components_Datastory_HouseHoldBills_jsx\")]).then(__webpack_require__.bind(__webpack_require__, /*! ../../components/Datastory.HouseHoldBills */ \"./js/components/Datastory.HouseHoldBills.jsx\"));\n});\nvar Footer = /*#__PURE__*/(0,react__WEBPACK_IMPORTED_MODULE_0__.lazy)(function () {\n  return Promise.all(/*! import() */[__webpack_require__.e(\"vendors-node_modules_reactstrap_es_Fade_js-node_modules_babel_runtime_helpers_esm_assertThisI-9a4f1c\"), __webpack_require__.e(\"vendors-node_modules_react-popper_lib_esm_Popper_js-node_modules_reactstrap_es_Button_js\"), __webpack_require__.e(\"vendors-node_modules_reactstrap_es_Popover_js-node_modules_reactstrap_es_PopoverBody_js-node_-72dee4\"), __webpack_require__.e(\"js_components_Footer_jsx\")]).then(__webpack_require__.bind(__webpack_require__, /*! ../../components/Footer */ \"./js/components/Footer.jsx\"));\n});\nvar EPW = function EPW() {\n  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(\"div\", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(Navigation_scoda, {\n    box_shadow: true\n  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(DataStory, null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(Footer, null));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (EPW);\n\n//# sourceURL=webpack://ocl_bb_library/./js/templates/datastories/DatastoryHouseholdBills.jsx?");
+
+/***/ })
+
+}]);
